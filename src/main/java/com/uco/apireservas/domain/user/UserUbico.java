65b555182e0fae6d5e97a70314223a343a03fd56
Mrced <marcedlll12@gmail.com>
2023-04-19 11:38:30 -0500

    Mapeo agregado al domain de los campos corresppndientes en base de datos

--- src/main/java/com/uco/apireservas/domain/user/UserUbico.java
+++ src/main/java/com/uco/apireservas/domain/user/UserUbico.java
@@ -1,10 +1,11 @@
 package com.uco.apireservas.domain.user;
 
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.*;;
 
 @Entity
 @Table(name = "usuarios")
@@ -14,17 +15,34 @@ public class UserUbico {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id", nullable = false)
+        @Column(name = "id", nullable = false)
     private Long id;
 
+    @Column(name = "name")
     private String name;
+
+    @Column(name = "lastname")
     private String lastname;
+
+    @Column(name = "identificationType")
     private String identificationType;//Verificar si es aca o en UserType
+
+    @Column(name = "identificationNumber")
     private long identificationNumber;
+
+    @Column(name = "telephoneNumber")
     private long telephoneNumber;
+
+    @Column(name = "Email")
     private String Email;
+
+    @Column(name = "ucoid")
     private long ucoid;
+
+    @Column(name = "username")
     private String username;
+
+    @Column(name = "password")
     private String password;
 
     public UserUbico(String name, String lastname, String identificationType, long identificationNumber,
@@ -41,82 +59,5 @@ public class UserUbico {
     }
 
     public UserUbico() {
-    }
-
-
-    public static Integer getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getLastname() {
-        return lastname;
-    }
-
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
-
-    public String getIdentificationType() {
-        return identificationType;
-    }
-
-    public void setIdentificationType(String identificationType) {
-        this.identificationType = identificationType;
-    }
-
-    public long getIdentificationNumber() {
-        return identificationNumber;
-    }
-
-    public void setIdentificationNumber(long identificationNumber) {
-        this.identificationNumber = identificationNumber;
-    }
-
-    public long getTelephoneNumber() {
-        return telephoneNumber;
-    }
-
-    public void setTelephoneNumber(long telephoneNumber) {
-        this.telephoneNumber = telephoneNumber;
-    }
-
-    public String getEmail() {
-        return Email;
-    }
-
-    public void setEmail(String email) {
-        Email = email;
-    }
-
-    public long getUcoid() {
-        return ucoid;
-    }
-
-    public void setUcoid(long ucoid) {
-        this.ucoid = ucoid;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
     }
 }
