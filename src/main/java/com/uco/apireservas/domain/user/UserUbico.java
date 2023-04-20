13b74d9f30365186974adea3c5e1a41aca2a360e
Mrced <marcedlll12@gmail.com>
2023-04-19 20:38:56 -0500

    Correcciòn JwtUserService

--- src/main/java/com/uco/apireservas/domain/user/UserUbico.java
+++ src/main/java/com/uco/apireservas/domain/user/UserUbico.java
@@ -58,6 +58,86 @@ public class UserUbico {
         this.password = password;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
     public UserUbico() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getIdentificationType() {
+        return identificationType;
+    }
+
+    public void setIdentificationType(String identificationType) {
+        this.identificationType = identificationType;
+    }
+
+    public long getIdentificationNumber() {
+        return identificationNumber;
+    }
+
+    public void setIdentificationNumber(long identificationNumber) {
+        this.identificationNumber = identificationNumber;
+    }
+
+    public long getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(long telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public long getUcoid() {
+        return ucoid;
+    }
+
+    public void setUcoid(long ucoid) {
+        this.ucoid = ucoid;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
